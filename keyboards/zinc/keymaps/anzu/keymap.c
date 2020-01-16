d3b84e6f6dea7f4299f7ac30f71e5f1bf9b5457f
anzu takahashi <anzu@happy-girl.jp>
2020-01-17 00:37:44 +0900

    anzu zinc ADJUST GUI入れ替え

--- keyboards/zinc/keymaps/anzu/keymap.c
+++ keyboards/zinc/keymaps/anzu/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
       KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       CTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_LCBR, KC_RCBR, \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                   KC_N,    KC_M,    KC_SCLN, KC_QUOT, KC_UP,   KC_SLSH, \
-      ADJUST, KC_LALT, KC_LGUI, KC_MINS, KC_UNDS,  LT(_RAISE, KC_SPC),     KC_ENT,  KC_COMM, KC_DOT , KC_LEFT, KC_DOWN, KC_RGHT \
+      KC_LGUI, KC_LALT, ADJUST, KC_MINS, KC_UNDS,  LT(_RAISE, KC_SPC),     KC_ENT,  KC_COMM, KC_DOT , KC_LEFT, KC_DOWN, KC_RGHT \
       ),
 
 
