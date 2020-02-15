76fbc1592a8092ea3cabe072663e0d1f4fa0fae6
anzu takahashi <anzu@happy-girl.jp>
2020-02-15 23:21:00 +0900

    anzu: nikoro10 added

---
+++ keyboards/nikoro10/keymaps/default/config.h
@@ -0,0 +1,40 @@
+/* Copyright 2020 anzu takahashi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// place overrides here
+
+// Selection of RGBLIGHT MODE to use.
+#if defined(LED_ANIMATIONS)
+   //#define RGBLIGHT_EFFECT_BREATHING
+   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+   //#define RGBLIGHT_EFFECT_SNAKE
+   #define RGBLIGHT_EFFECT_KNIGHT
+   //#define RGBLIGHT_EFFECT_CHRISTMAS
+   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+   //#define RGBLIGHT_EFFECT_RGB_TEST
+   //#define RGBLIGHT_EFFECT_ALTERNATING
+#endif
+
+// LED Setting: if you have KUMO you can use RGBLIGHT_ENABLE = yes
+#ifdef RGBLIGHT_ENABLE
+  #define RGB_DI_PIN B2
+  #define RGBLIGHT_TIMER
+
+  #define RGBLED_NUM 6
+#endif
