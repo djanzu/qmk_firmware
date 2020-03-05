983122827bb6301442a1664ee85bb17908815fe2
Anzu Takahashi <anzu_takahashi@queserser.co.jp>
2020-03-05 17:12:01 +0900

    cocoa40: hhkb us rgb

--- keyboards/cocoa40/keymaps/hhkb_anzu/config.h
+++ keyboards/cocoa40/keymaps/hhkb_anzu/config.h
@@ -18,3 +18,24 @@
 
 // place overrides here
 
+
+// Selection of RGBLIGHT MODE to use.
+#if defined(LED_ANIMATIONS)
+   //#define RGBLIGHT_EFFECT_BREATHING
+   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
+  //  #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+   //#define RGBLIGHT_EFFECT_SNAKE
+   #define RGBLIGHT_EFFECT_KNIGHT
+   //#define RGBLIGHT_EFFECT_CHRISTMAS
+   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
+   //#define RGBLIGHT_EFFECT_RGB_TEST
+   //#define RGBLIGHT_EFFECT_ALTERNATING
+#endif
+
+// LED Setting: if you have KUMO you can use RGBLIGHT_ENABLE = yes
+#ifdef RGBLIGHT_ENABLE
+  #define RGB_DI_PIN B2
+  #define RGBLIGHT_TIMER
+
+  #define RGBLED_NUM 6
+#endif
