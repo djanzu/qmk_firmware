ba467fc2cb9bde1fee6694803fffb81a8ad46fee
あんず <>
2020-04-17 01:30:54 +0900

    anzu: bm43a

--- keyboards/bm43a/keymaps/anzu2/keymap.c
+++ keyboards/bm43a/keymaps/anzu2/keymap.c
@@ -18,9 +18,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        L_CTL(KC_TAB),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
+        LCTL(KC_TAB),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_UP,   KC_DOT,
-        MO(2),  KC_LGUI, KC_LALT, LT(2, KC_SPC),                 KC_SPC,              MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LGUI, KC_LALT, KC_GRV,  LT(2, KC_SPC),             LT(2, KC_SPC),              MO(1),   KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [1] = LAYOUT(
         RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -30,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [2] = LAYOUT(
 		_______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, _______, _______, _______, KC_SCLN, KC_QUOT, _______,
+        _______, KC_CAPS, _______, _______, _______, _______, _______, _______,  KC_GRV, _______, KC_SLSH,
         _______, _______, _______, _______,                _______,             _______, _______, _______, _______
     ),
 
