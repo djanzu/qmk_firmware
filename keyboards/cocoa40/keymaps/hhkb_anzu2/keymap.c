7de83028f682596865e944cefa85407718449367
Anzu Takahashi <anzu_takahashi@queserser.co.jp>
2020-04-30 00:17:56 +0900

    muemue

--- keyboards/cocoa40/keymaps/hhkb_anzu2/keymap.c
+++ keyboards/cocoa40/keymaps/hhkb_anzu2/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GESC, KC_Q, KC_W, KC_E, LT(_RGB, KC_R), KC_T,  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSPACE,
     KC_LCTRL, KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_BSLASH,
     KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N,    KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, LT(_FN, KC_GRV),
-    KC_LGUI, LALT_T(KC_TAB),  KC_SPACE,               KC_ENTER, KC_RGUI, MO(_NUMS)
+    KC_LGUI, LALT_T(KC_TAB), LT(_NUMS, KC_SPACE),               KC_ENTER, KC_RGUI, MO(_NUMS)
   ),
   [_NUMS] = LAYOUT(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, _______,
@@ -44,13 +44,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN] = LAYOUT(
     KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_UP, KC_F12, KC_DELETE,
     _______, KC_F11, _______, _______, _______,  _______, KC_PGUP, _______, _______, _______, KC_LEFT, KC_RIGHT, _______,
-    _______, _______, _______, _______, _______, _______, KC_PGDOWN, _______, _______, _______, KC_DOWN, _______,  _______, 
+    _______, _______, _______, _______, _______, _______, KC_PGDOWN, _______, _______, _______, KC_DOWN, _______,  _______,
     RESET, _______, _______, _______, _______, _______
   ),
   [_RGB] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, RGB_MOD, RGB_HUD, RGB_HUI, RGB_TOG, _______, _______,  _______, _______, _______, _______, _______,  _______,
-    _______, _______, RGB_SAD, RGB_SAI, _______, _______, _______,  _______, _______, _______, _______, _______, _______, 
-    RESET, _______, _______, _______, _______, _______
+    _______, _______, RGB_SAD, RGB_SAI, _______, _______, _______,  _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______
   ),
 };
