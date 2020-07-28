ee748f81bf7c95f9edb24a9d131ef2106a82c3bb
anzu takahashi <anzu@happy-girl.jp>
2020-07-29 03:29:06 +0900

    add: anzu hhkb4

---
+++ keyboards/cocoa40/keymaps/hhkb_anzu4/keymap.c
@@ -0,0 +1,56 @@
+/* Copyright 2019 'Naoto Takai'
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+enum layer_names {
+	_BASE,
+	_NUMS,
+	_SYMS,
+	_FN,
+  _RGB
+};
+
+#define KC_NUMS LT(_NUMS, KC_SPACE)
+#define KC_SYMS LT(_SYMS, KC_SPACE)
+#define KC_FN MO(_FN)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT(
+    KC_GESC, KC_Q, KC_W, KC_E, LT(_RGB, KC_R), KC_T,  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSPACE,
+    LCTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER,
+    KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N,    KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, MO(_FN),
+    KC_GRV, KC_LALT, LGUI_T(KC_SPACE),               LGUI_T(KC_SPACE), MO(_NUMS), KC_BSLASH
+  ),
+  [_NUMS] = LAYOUT(
+    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSLASH,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, MO(_RGB), _______, _______, _______
+  ),
+  [_FN] = LAYOUT(
+    KC_ESC,  KC_F1, KC_F2,  KC_F3,  KC_F4,  _______, _______, _______, _______, _______,   _______, KC_UP, _______, KC_DELETE,
+    _______, KC_F5, KC_F6,  KC_F7,  KC_F8,  _______, _______, _______, KC_HOME, KC_PGUP,   KC_LEFT, KC_RIGHT, _______,
+    _______, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, _______, KC_END,  KC_PGDOWN, KC_DOWN, _______,  _______, 
+    _______, _______, _______, _______, _______, _______
+  ),
+  [_RGB] = LAYOUT(
+    RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    _______, RGB_MOD, RGB_HUD, RGB_HUI, RGB_TOG, _______, _______,  _______, _______, _______, _______, _______,  _______,
+    _______, _______, RGB_SAD, RGB_SAI, _______, _______, _______,  _______, _______, _______, _______, _______, _______, 
+    _______, _______, _______, _______, _______, _______
+  ),
+};
