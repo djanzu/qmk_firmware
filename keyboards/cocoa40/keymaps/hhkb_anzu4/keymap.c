86bcfc1e80fe1c1253a40979a3548820ec533128
anzu takahashi <anzu@happy-girl.jp>
2021-02-25 00:36:08 +0900

    cocoa

--- keyboards/cocoa40/keymaps/hhkb_anzu4/keymap.c
+++ keyboards/cocoa40/keymaps/hhkb_anzu4/keymap.c
@@ -30,20 +30,20 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_GESC, KC_Q, KC_W, KC_E, LT(_RGB, KC_R), KC_T,  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSPACE,
-    LCTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER,
+    KC_GESC, KC_Q, KC_W, KC_E, LT(_RGB, KC_R), KC_T,  KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_GRV,
+    LCTL_T(KC_TAB), KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_BSPACE,
     KC_LSHIFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N,    KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, MO(_FN),
-    KC_GRV, KC_LALT, LGUI_T(KC_SPACE),               LGUI_T(KC_SPACE), MO(_NUMS), KC_BSLASH
+    KC_GRV, KC_LALT, LGUI_T(KC_SPACE),               LT(_NUMS, KC_ENTER), MO(_NUMS), KC_BSLASH
   ),
   [_NUMS] = LAYOUT(
     KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSLASH,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_4, KC_5, KC_6, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, KC_7, KC_8, KC_9, KC_0, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, MO(_RGB), _______, _______, _______
   ),
   [_FN] = LAYOUT(
-    KC_ESC,  KC_F1, KC_F2,  KC_F3,  KC_F4,  _______, _______, _______, _______, _______,   _______, KC_UP, _______, KC_DELETE,
-    _______, KC_F5, KC_F6,  KC_F7,  KC_F8,  _______, _______, _______, KC_HOME, KC_PGUP,   KC_LEFT, KC_RIGHT, _______,
+    KC_ESC,  KC_F1, KC_F2,  KC_F3,  KC_F4,  _______, _______, _______, _______, _______,   _______, KC_UP, _______, MO(_RGB),
+    _______, KC_F5, KC_F6,  KC_F7,  KC_F8,  _______, _______, _______, KC_HOME, KC_PGUP,   KC_LEFT, KC_RIGHT, KC_DELETE,
     _______, KC_F9, KC_F10, KC_F11, KC_F12, _______, _______, _______, KC_END,  KC_PGDOWN, KC_DOWN, _______,  _______, 
     _______, KC_LANG1, KC_LANG2, _______, _______, _______
   ),
