72dcde86b61c6e4c60c6088ee417ac61ca53f50f
anzu takahashi <anzu@happy-girl.jp>
2021-03-06 13:21:49 +0900

    少し変更

--- keyboards/yarinaoshi/keymaps/default/keymap.c
+++ keyboards/yarinaoshi/keymaps/default/keymap.c
@@ -37,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //     KC_A, KC_B, KC_C, KC_D,
     //     KC_E, KC_F, KC_G, KC_H,
     //     KC_I, KC_J, KC_K, KC_L,
-    // ),
+    // )
     [_BASE] = LAYOUT( 
-        RGB_TOG, KC_B, KC_C, KC_D,
+        RGB_TOG, KC_NO, KC_NO, KC_NO,
         KC_NO, KC_NO, RGB_VAI, RGB_VAD, 
         RGB_SAI, RGB_SAD, RGB_HUI, RGB_HUD, 
     )
